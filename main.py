6fe0db0a4dd776783ec174c10d7cd0fac984901b
IDKJW <124316245+IDKJW@users.noreply.github.com>
2023-05-19 12:04:26 +1200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+#Import modules
+import tkinter
+from tkinter import *
+import os
+import customtkinter
+from PIL import ImageTk, Image
+ 
+# Designing the main screen
+def main_screen():
+    global root
+    root = Tk()
+    root.geometry("500x800")
+    main_frame = Frame(root,bg ='#020202')
+    main_frame.pack(padx=20)
+    main_frame.pack()
+    background_image = PhotoImage(file="assets/Aim high.png")
+    background_label = Label(root,image = background_image)
+    background_label.place(x=0, y=0, relwidth = 1 , relheight = 1)
+    root.title("Account Login")
+    top_text=Label(root,text="Welcome to FitQuest",bg="#020202",font=("Calibri", 45))
+    top_text.pack(pady=0, anchor =tkinter.CENTER)
+    #Login button 
+    Login_button = customtkinter.CTkButton(master=root, text="Login", command=login, height = 35, width = 250,bg_color="#020202")
+    Login_button.place(relx=0.48, rely=0.3, anchor=tkinter.CENTER)
+    #register button
+    register_button = customtkinter.CTkButton(master=root, text="Register", command=register,height = 35, width = 250, bg_color="#020202" )
+    register_button.place(relx=0.48, rely=0.4, anchor=tkinter.CENTER)
+    #Logo act as a button 
+    logo = ImageTk.PhotoImage(Image.open("assets/Logo.png"),Image.ANTIALIAS)
+    button1 =customtkinter.CTkButton(master=root,image=logo,text="",compound="left" , hover=False, bg_color="#020202")
+    button1.place(relx=0.48, rely=0.158, anchor=tkinter.CENTER)
+    
+ 
+    root.mainloop()
+
+def login():
+    pass
+
+def register():
+    pass
+
+main_screen()
