528c29f2c76f79e2bf30be4fc40f4bf8e1e22a5f
IDKJW <124316245+IDKJW@users.noreply.github.com>
2023-06-16 11:37:04 +1200

    workout.py

---
+++ workout.py
@@ -0,0 +1,13 @@
+#Import modules
+import tkinter
+from tkinter import *
+import os
+import customtkinter
+from PIL import ImageTk, Image
+
+workout = Tk()
+workout.title("Workout")
+workout.geometry("500x800")
+
+
+workout()
