719d1b40e3e1c76a0b03da49afbd07f25b5f454f
IDKJW <124316245+IDKJW@users.noreply.github.com>
2023-08-12 20:19:51 +1200

    Update shoulder.py

--- shoulder.py
+++ shoulder.py
@@ -25,9 +25,9 @@ class ShoulderExerciseApp:
 
         # Create a font
         self.custom_font = font.Font(
-            family="Helvetica", size=20, weight="bold")
-        self.small_custom_font = font.Font(
             family="Helvetica", size=15, weight="bold")
+        self.small_custom_font = font.Font(
+            family="Helvetica", size=13, weight="bold")
 
         # Create a canvas
         self.canvas = tk.Canvas(self.root, width=500, height=800)
