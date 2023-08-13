0a47d33a5d6b4a863b6647b6c2126c2b4b4e7fe8
IDKJW <124316245+IDKJW@users.noreply.github.com>
2023-08-13 15:59:26 +1200

    Update leg.py

--- leg.py
+++ leg.py
@@ -135,8 +135,9 @@ class LegExerciseApp:
                 text="All exercises have been shown.")
             self.canvas.itemconfig(self.reps_label, text="")
             self.canvas.itemconfig(self.image_label, image="")
+            # Change the button text after all the excerise has been genertated
             self.generate_button.config(
-                text="Well Done")  # Change the button text
+                text="Well Done")
             return
 
         # Get a random exercise that hasn't been shown yet
