a84162d9125cb6a1c6b567d29fe96241b19f5210
IDKJW <124316245+IDKJW@users.noreply.github.com>
2023-08-13 15:58:48 +1200

    Update cardio.py

--- cardio.py
+++ cardio.py
@@ -134,8 +134,9 @@ class CardioExerciseApp:
                 text="All exercises have been shown.")
             self.canvas.itemconfig(self.reps_label, text="")
             self.canvas.itemconfig(self.image_label, image="")
+            # Change the button text after all the excerise has been genertated
             self.generate_button.config(
-                text="Well Done")  # Change the button text
+                text="Well Done")
             return
 
         # Get a random exercise that hasn't been shown yet
